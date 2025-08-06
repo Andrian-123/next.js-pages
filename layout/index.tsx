aad859d97e1a8002dcf25bcb28ae2f469c3f2fb3
Andrian <andrianiskandarwork@gmail.com>
2025-08-06 20:03:33 +0700

    Tugas hari-3

--- layout/index.tsx
+++ layout/index.tsx
@@ -41,6 +41,9 @@ export default function RootLayout({
               <li>
                 <Link href="/about">About</Link>
               </li>
+              <li>
+                <Link href="/user">User</Link>
+              </li>
             </ul>
           </div>
         </header>
